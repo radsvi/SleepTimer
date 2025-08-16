7accc64add6d9c1a274a0c38aa81f423f24ec597
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-08-16 12:32:14 +0200

    Adding MVVM toolkit

---
+++ SleepTimer/GlobalUsings.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//global using CommunityToolkit.Mvvm.ComponentModel;
+//global using CommunityToolkit.Mvvm.Input;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+//global using System.Text.Json;
+//global using Newtonsoft.Json;
+
+
+
+//global using SleepTimer.Behaviors;
+//global using SleepTimer.Converters;
+//global using SleepTimer.Enums;
+//global using SleepTimer.Helpers;
+//global using SleepTimer.Models;
+//global using SleepTimer.MVVM;
+//global using SleepTimer.Services;
+//global using SleepTimer.ViewModels;
+//global using SleepTimer.Views;
+//global using SleepTimer.Views.Controls;
