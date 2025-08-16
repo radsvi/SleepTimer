6390349f8932102e92e86ea448f2d4b5e602e3c4
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-08-16 16:12:26 +0200

    Implementing MainTimer class

--- SleepTimer/Models/MainTimer.cs
+++ SleepTimer/Models/MainTimer.cs
@@ -7,32 +7,60 @@ using System.Timers;
 
 namespace SleepTimer.Models
 {
-    public class MainTimer
+    public class MainTimer : ObservableObject
     {
-        public System.Timers.Timer TimerToken { get; private set; } = new System.Timers.Timer();
-        public MainTimer()
+        readonly AppPreferences appPreferences;
+        public MainTimer(AppPreferences appPreferences)
         {
-            TimerToken.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            TimerToken.Interval = 5000; // ~ 5 seconds
-            
+
+            this.appPreferences = appPreferences;
+
+            //Timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            Timer.Elapsed += OnTimedEvent;
+            Timer.Interval = 1000; // seconds
         }
-        //public bool IsStarted { get; private set; }
+        public System.Timers.Timer Timer { get; private set; } = new System.Timers.Timer();
+        public DateTime? EndTime { get; private set; }
+        private bool isStarted;
+        public bool IsStarted
+        {
+            get => isStarted;
+            set { isStarted = value; OnPropertyChanged(); }
+        }
+        private TimeSpan remainingTime = TimeSpan.MinValue;
+        public TimeSpan RemainingTime
+        {
+            get => remainingTime;
+            set { remainingTime = value; OnPropertyChanged(); }
+        }
 
         private void OnTimedEvent(object? source, ElapsedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (EndTime != null)
+                RemainingTime = (DateTime)EndTime - e.SignalTime;
         }
         public void Start()
         {
-            TimerToken.Enabled = true;
+            Timer.Enabled = true;
+            IsStarted = true;
+            EndTime = DateTime.Now.AddMinutes(appPreferences.DefaultDuration);
+
+            if (EndTime != null)
+                RemainingTime = (DateTime)EndTime - DateTime.Now;
         }
         public void Stop()
         {
-            TimerToken.Enabled = false;
+            Timer.Enabled = false;
+            IsStarted = false;
+            EndTime = null;
         }
         public void Extend()
         {
-            throw new NotImplementedException();
+            if (EndTime is null) throw new InvalidOperationException();
+
+            DateTime dateTime = (DateTime)EndTime;
+
+            EndTime = dateTime.AddMinutes(appPreferences.ExtensionLength);
         }
     }
 }
--- SleepTimer/ViewModels/MainVM.cs
+++ SleepTimer/ViewModels/MainVM.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace SleepTimer.ViewModels
 {
-    public partial class MainVM(AppPreferences appPreferences, MainTimer mainTimer) : ObservableObject
+    public partial class MainVM : ObservableObject
     {
-        public AppPreferences AppPreferences { get; } = appPreferences;
-        public MainTimer MainTimer { get; } = mainTimer;
+        public AppPreferences AppPreferences { get; }
+        public MainTimer MainTimer { get; }
 
+        public MainVM(AppPreferences appPreferences, MainTimer mainTimer)
+        {
+            AppPreferences = appPreferences;
+            MainTimer = mainTimer;
+
+            MainTimer.PropertyChanged += MainTimer_PropertyChanged;
+        }
+
+        private void MainTimer_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MainTimer.IsStarted))
+            {
+                ExtendTimerCommand.NotifyCanExecuteChanged();
+            }
+        }
 
         [RelayCommand]
         async Task NavigateToPage(Pages page)
@@ -33,6 +48,20 @@ namespace SleepTimer.ViewModels
         void StartTimer()
         {
             MainTimer.Start();
+        }
+        [RelayCommand]
+        void StopTimer()
+        {
+            MainTimer.Stop();
+        }
+        [RelayCommand(CanExecute = nameof(IsStarted))]
+        void ExtendTimer()
+        {
+            MainTimer.Extend();
+        }
+        bool IsStarted()
+        {
+            return MainTimer.IsStarted;
         }
     }
 }
