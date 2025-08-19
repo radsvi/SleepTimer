d4722cd8c4d83b3de279d18472c6d38fd055bed3
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-08-19 13:12:52 +0200

    Adding Local Notification - removing from codebehind

--- SleepTimer/Views/MainPage.xaml.cs
+++ SleepTimer/Views/MainPage.xaml.cs
@@ -10,58 +10,5 @@ namespace SleepTimer.Views
 
             BindingContext = mainVM;
         }
-
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            var notification = new NotificationRequest
-            {
-                NotificationId = 100,
-                Title = "Hello!",
-                Subtitle = "subtitle",
-                Description = "This is a local notification from MAUI",
-                ReturningData = "Some data",
-                BadgeNumber = 42,
-                
-            };
-            //Android = new Plugin.LocalNotification.AndroidOption.AndroidOptions
-            //{
-            //    ChannelId = "default",
-
-            //}
-            //ChannelId = "default",
-            //Importance = AndroidImportance.High,
-            //Priority = AndroidPriority.High,
-            //Visibility = AndroidVisibilityType.Public
-
-            //#if ANDROID
-            //            notification.Android = new Plugin.LocalNotification.AndroidOption.AndroidOptions
-            //            {
-            //                ChannelId = "default",
-            //                Priority = Plugin.LocalNotification.AndroidOptionPriority.High
-            //            };
-            //#endif
-
-            //NotifyTime = DateTime.Now.AddSeconds(5) // Schedule for 5 seconds later
-            //var qwer = LocalNotificationCenter.Current.AreNotificationsEnabled;
-            await EnsureNotificationPermissionAsync();
-            //await LocalNotificationCenter.Current.RequestNotificationPermission();
-            LocalNotificationCenter.Current.Show(notification);
-        }
-        public async Task<bool> EnsureNotificationPermissionAsync()
-        {
-            // Returns true if notifications are allowed for your app
-            var enabled = await LocalNotificationCenter.Current.AreNotificationsEnabled();
-            if (enabled) return true;
-
-            // Android 13+ shows a runtime prompt; this triggers it.
-            // (Optionally request exact alarm if you schedule exact times)
-            var granted = await LocalNotificationCenter.Current.RequestNotificationPermission(
-                new NotificationPermission
-                {
-                    Android = { RequestPermissionToScheduleExactAlarm = false }
-                });
-
-            return granted;
-        }
     }
 }
