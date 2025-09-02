e4baf797f8566ed2440ed4bc9390dc943e108004
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-09-02 11:12:27 +0200

    Replacing magic string with enums

--- SleepTimer/Platforms/Android/SleepTimerService.cs
+++ SleepTimer/Platforms/Android/SleepTimerService.cs
@@ -5,6 +5,12 @@ using Android.Media;
 
 namespace SleepTimer.Platforms.Android
 {
+    public enum ServiceAction
+    {
+        Start,
+        Postpone,
+        Stop
+    }
     [Service(ForegroundServiceType = global::Android.Content.PM.ForegroundService.TypeMediaPlayback)]
     public class SleepTimerService : Service
     {
@@ -14,12 +20,12 @@ namespace SleepTimer.Platforms.Android
 
         public override StartCommandResult OnStartCommand(Intent? intent, StartCommandFlags flags, int startId)
         {
-            if (intent?.Action == "START")
+            if (intent?.Action == ServiceAction.Start.ToString())
             {
                 var minutes = intent.GetIntExtra("minutes", 5);
                 StartTimer(TimeSpan.FromMinutes(minutes));
             }
-            else if (intent?.Action == "POSTPONE")
+            else if (intent?.Action == ServiceAction.Postpone.ToString())
             {
                 var logic = ServiceHelper.GetService<ISleepTimerLogic>();
                 _ = logic.OnPostponeAsync();
@@ -27,7 +33,7 @@ namespace SleepTimer.Platforms.Android
                 // restart timer
                 StartTimer(TimeSpan.FromMinutes(10));
             }
-            else if (intent?.Action == "STOP")
+            else if (intent?.Action == ServiceAction.Stop.ToString())
             {
                 StopTimer();
                 StopSelf();
@@ -95,15 +101,15 @@ namespace SleepTimer.Platforms.Android
             }
 
             var postponeIntent = new Intent(this, typeof(SleepTimerService));
-            postponeIntent.SetAction("POSTPONE");
+            postponeIntent.SetAction(ServiceAction.Postpone.ToString());
             var postponePending = PendingIntent.GetService(this, 1, postponeIntent, PendingIntentFlags.Immutable);
 
             var stopIntent = new Intent(this, typeof(SleepTimerService));
-            stopIntent.SetAction("STOP");
+            stopIntent.SetAction(ServiceAction.Stop.ToString());
             var stopPending = PendingIntent.GetService(this, 2, stopIntent, PendingIntentFlags.Immutable);
 
-            builder.AddAction(new Notification.Action.Builder(0, "Postpone", postponePending).Build())
-                .AddAction(new Notification.Action.Builder(0, "Stop", stopPending).Build());
+            builder.AddAction(new Notification.Action.Builder(0, ServiceAction.Postpone.ToString(), postponePending).Build())
+                .AddAction(new Notification.Action.Builder(0, ServiceAction.Stop.ToString(), stopPending).Build());
 
             var notification = builder.Build();
 
