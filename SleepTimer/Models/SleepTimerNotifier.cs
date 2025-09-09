542a9b2990b6076d18e8d633d5e990881809e662
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-09-09 19:50:29 +0200

    MainTimer with SRP - fixing notifications

--- SleepTimer/Models/SleepTimerNotifier.cs
+++ SleepTimer/Models/SleepTimerNotifier.cs
@@ -9,7 +9,7 @@ namespace SleepTimer.Models
     public class SleepTimerNotifier
     {
         private readonly AppPreferences appPreferences;
-        private int lastNotificationMinute = int.MaxValue;
+        private TimeSpan? lastNotification;
         private readonly Action<string, NotificationLevel> notify;
 
         public SleepTimerNotifier(AppPreferences appPreferences, Action<string, NotificationLevel> notify)
@@ -20,18 +20,24 @@ namespace SleepTimer.Models
 
         public void OnTick(TimeSpan remainingTime)
         {
+            if (lastNotification == null)
+                lastNotification = remainingTime;
+
             if (remainingTime.TotalSeconds <= 0)
                 notify("Sleep timer finished.", NotificationLevel.Low);
             else if (remainingTime.Minutes == 0 && remainingTime.Seconds < 10)
                 notify("Going to sleep.", NotificationLevel.Low);
-            else if (remainingTime.Seconds > 0 && Math.Abs(remainingTime.Minutes - lastNotificationMinute) > 0)
+            else if (remainingTime.Seconds < 5 && Math.Abs((remainingTime - (TimeSpan)lastNotification).Seconds) <= 0)
             {
                 var highPriorityMinutes = new int[] { 1, 2, 5, 10 };
+                NotificationLevel chosenPriority;
                 if (highPriorityMinutes.Contains(remainingTime.Minutes))
-                {
-                    notify($"{remainingTime.Minutes} minutes left.", NotificationLevel.High);
-                    lastNotificationMinute = remainingTime.Minutes;
-                }
+                    chosenPriority = NotificationLevel.High;
+                else
+                    chosenPriority = NotificationLevel.Low;
+
+                notify($"{remainingTime.Minutes} minutes left.", chosenPriority);
+                lastNotification = remainingTime.Add(new TimeSpan(0,0,-55));
             }
         }
     }
