6868110918007b18313295e011ea6553e5cadf20
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2025-09-17 17:29:40 +0200

    Adding GroupBoxControl

---
+++ SleepTimer/Views/Controls/GroupBoxControl.xaml.cs
@@ -0,0 +1,17 @@
+namespace SleepTimer.Views.Controls;
+
+public partial class GroupBoxControl : ContentView
+{
+
+    public GroupBoxControl()
+    {
+        InitializeComponent();
+    }
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    public static readonly BindableProperty HeaderProperty = BindableProperty.Create(nameof(Header), typeof(string), typeof(GroupBoxControl), string.Empty);
+
+}
